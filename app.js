9a87926274b8870d483d283e0eb0fd26dddec405
Hiroshi Kawaguchi <hi.kwgc@gmail.com>
2017-11-12 21:38:17 +0900

    enabled to output as multi reports

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ app.set('views', __dirname + '/views');
 const client_id = process.env.GENOMELINK_CLIENT_ID;
 const client_secret = process.env.GENOMELINK_CLIENT_SECRET;
 const redirect_uri = process.env.GENOMELINK_CALLBACK_URL;
-const scope = 'report:eye-color';
+const scope = 'report:eye-color report:beard-thickness report:morning-person';
 // const scope = 'report:eye-color report:beard-thickness report:morning-person';
-const authorize_url = `http://localhost:8000/oauth/authorize?redirect_uri=${redirect_uri}&client_id=${client_id}&response_type=code&scope=${scope}`;
+const authorize_url = `https://genomelink.io/oauth/authorize?redirect_uri=${redirect_uri}&client_id=${client_id}&response_type=code&scope=${scope}`;
 
 app.get('/', function(req, res) {
   res.render('index', {
     authorize_url: authorize_url,
-    report: undefined,
+    reports: undefined,
   });
 });
 
@@ -26,7 +26,7 @@ app.get('/callback', function(req, res) {
   const authorization_code = req.query.code;
 
   request.post({
-    url: 'http://localhost:8000/oauth/token',
+    url: 'https://genomelink.io/oauth/token',
     json: true,
     form: {
       grant_type:    'authorization_code',
@@ -43,14 +43,17 @@ app.get('/callback', function(req, res) {
     // At this point you can fetch protected resources.
     // So, fetch a protected resource using an OAuth2 token.
     const token =  body.access_token;
-    // const report = [];
+    const scopes = body.scope.split(' ');
+
+    const reports = [];
 
     // TODO: call multiple reports
-    ['eye-color'].forEach((name, idx) => {
+    scopes.forEach((name, idx) => {
+      name = name.replace(/report:/g, '');
 
       // TODO: async/await foreach API call (?)
       request.get({
-        url: `http://localhost:8000/v1/reports/${name}?population=european`,
+        url: `https://genomelink.io/v1/reports/${name}?population=european`,
         json: true,
         headers: {
           'Authorization': `Bearer ${token}`
@@ -59,13 +62,14 @@ app.get('/callback', function(req, res) {
         if (err) {
           return console.error('failed to fetch report:', err);
         }
-
+        reports.push(body);
         // TODO: move outside of this callback
-        res.render('index', {
-          authorize_url: authorize_url,
-          report: JSON.stringify(body)
-        });
-
+        if (idx === scopes.length - 1) {
+          res.render('index', {
+            authorize_url: authorize_url,
+            reports: reports
+          });
+        }
       });
     });
 
