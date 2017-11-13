fe1bbec72dd2f0cad5fb41f1d8a7a8296a986ba9
Hiroshi Kawaguchi <hi.kwgc@gmail.com>
2017-11-13 16:59:29 +0900

    enabled session

--- app.js
+++ app.js
@@ -1,40 +1,52 @@
 const express = require('express');
+const session = require('express-session');
 const genomeLink = require('genomelink-node');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-const scope = 'report:eye-color report:beard-thickness report:morning-person';
+app.use(session({
+  secret: 'YOURSECRET',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 30 * 60 * 1000
+  }
+}));
 
-app.get('/', function(req, res) {
+app.get('/', async (req, res) => {
+  const scope = 'report:eye-color report:beard-thickness report:morning-person';
   const authorizeUrl = genomeLink.OAuth.authorizeUrl({ scope: scope });
+
+  // Fetching a protected resource using an OAuth2 token if exists.
+  let reports = [];
+  if (req.session.oauthToken) {
+    const scopes = scope.split(' ');
+    reports = await Promise.all(scopes.map( async (name) => {
+      return await genomeLink.Report.fetch({
+        name: name.replace(/report:/g, ''),
+        population: 'european',
+        token: req.session.oauthToken
+      });
+    }));
+  }
 
   res.render('index', {
     authorize_url: authorizeUrl,
-    reports: undefined,
+    reports: reports,
   });
 });
 
-app.get('/callback', async function(req, res) {
+app.get('/callback', async (req, res) => {
   // The user has been redirected back from the provider to your registered
   // callback URL. With this redirection comes an authorization code included
   // in the request URL. We will use that to obtain an access token.
-  const token = await genomeLink.OAuth.token({ requestUrl: req.url });
-  const scopes = scope.split(' ');
-  const reports = await Promise.all(scopes.map( async (name) => {
-    name = name.replace(/report:/g, '');
-    return await genomeLink.Report.fetch({
-      name: name,
-      population: 'european',
-      token: token
-    });
-  }));
+  req.session.oauthToken = await genomeLink.OAuth.token({ requestUrl: req.url });
 
-  res.render('index', {
-    authorize_url: null,
-    reports: reports
-  });
+  // At this point you can fetch protected resources but lets save
+  // the token and show how this is done from a persisted token in index page.
+  res.redirect('/');
 });
 
 // Run local server on port 3000.
